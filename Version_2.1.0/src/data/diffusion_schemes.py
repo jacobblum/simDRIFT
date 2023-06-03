45d0769ca93c96f90ed93a4cd30a8ce70690307f
kainenutt <55892455+kainenutt@users.noreply.github.com>
2023-06-03 14:16:26 -0500

    Include additional diffusion schemes

--- Version_2.1.0/src/data/diffusion_schemes.py
+++ Version_2.1.0/src/data/diffusion_schemes.py
@@ -10,6 +10,10 @@ def get(*args):
     for arg in args:
         if arg == 'DBSI_99':
             return _DBSI_99() 
+        elif arg == 'ABCD_102':
+            return _ABCD_102()
+        elif arg == 'NODDI':
+            return _NODDI()
 
 
 def _DBSI_99():
@@ -22,6 +26,39 @@ def _DBSI_99():
     except:
         bvals = np.loadtxt(path + os.sep + 'bval99', delimiter=',')
         bvecs = np.loadtxt(path + os.sep + 'bvec99', delimiter=',').T
+
+    div_safe = [not np.all(bvecs[i,:] == 0) for i in range(bvecs.shape[0])]
+    bvecs[div_safe] = bvecs[div_safe] / np.linalg.norm(bvecs[div_safe], ord = 2, axis = 1)[:, None]
+
+    return bvals, bvecs
+
+
+def _ABCD_102():
+    path, _ = os.path.split(os.path.realpath(__file__)) 
+    
+    try:
+        bvals = np.loadtxt(path + os.sep + 'bval_ABCD')
+        bvecs = np.loadtxt(path + os.sep + 'bvec_ABCD').T
+
+    except:
+        bvals = np.loadtxt(path + os.sep + 'bval_ABCD', delimiter=',')
+        bvecs = np.loadtxt(path + os.sep + 'bvec_ABCD', delimiter=',').T
+
+    div_safe = [not np.all(bvecs[i,:] == 0) for i in range(bvecs.shape[0])]
+    bvecs[div_safe] = bvecs[div_safe] / np.linalg.norm(bvecs[div_safe], ord = 2, axis = 1)[:, None]
+
+    return bvals, bvecs
+
+def _NODDI():
+    path, _ = os.path.split(os.path.realpath(__file__)) 
+    
+    try:
+        bvals = np.loadtxt(path + os.sep + 'bval_NODDI')
+        bvecs = np.loadtxt(path + os.sep + 'bvec_NODDI').T
+
+    except:
+        bvals = np.loadtxt(path + os.sep + 'bval_NODDI', delimiter=',')
+        bvecs = np.loadtxt(path + os.sep + 'bvec_NODDI', delimiter=',').T
 
     div_safe = [not np.all(bvecs[i,:] == 0) for i in range(bvecs.shape[0])]
     bvecs[div_safe] = bvecs[div_safe] / np.linalg.norm(bvecs[div_safe], ord = 2, axis = 1)[:, None]
