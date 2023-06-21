eecb0e6c8bf53644039d502d27430e0059876ab3
jacobblum <77986408+jacobblum@users.noreply.github.com>
2023-06-21 10:09:03 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 import setuptools
 
-install_requires  = ['numba', 'numpy', 'nibabel', 'dipy']
+install_requires  = ['numba', 'numpy', 'nibabel', 'dipy', 'pytest']
 setuptools.setup(
     name='simDRIFT',
     description='A software package for forward simulating diffusion weighted MRI',
