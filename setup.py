8f82a464a2a909641263497953f18b369d0751fe
jacobblum <77986408+jacobblum@users.noreply.github.com>
2023-06-21 10:09:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires  = ['numba', 'numpy', 'nibabel', 'dipy', 'pytest']
 setuptools.setup(
     name='simDRIFT',
     description='A software package for forward simulating diffusion weighted MRI',
-    url='https://github.com/jacobblum/dMRI-MCSIM',
+    url='https://github.com/jacobblum/simDRIFT',
     author='Jacob Blum, Kainen Utt',
     license='BSD (3-Clause)',
     packages=setuptools.find_packages(),
