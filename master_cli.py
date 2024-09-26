5b12264aacd10d815bc7700798c1833de245e842
Jacob Blum <77986408+jacobblum@users.noreply.github.com>
2024-09-26 10:21:37 -0700

    Add files via upload

---
+++ master_cli.py
@@ -0,0 +1,102 @@
+
+import sys
+import argparse
+from abc import ABC, abstractmethod
+from typing import Dict
+import importlib
+
+
+TOOL_NAME_LIST = ['src']
+
+class AbstractCLI(ABC):
+    """ Abstract class for simDRIFT command-line interface tools.
+    
+    Note:
+        Tools are called from the command line using
+        simDRIFT TOOL-NAME --optional_arg1 optional_val1 ...
+    
+    """
+
+    @abstractmethod
+    def get_name(self) -> str:
+        """Return the command-line name of the tool."""
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def validate_args(parser : argparse) -> argparse.Namespace:
+        """Do tool-specific argument validation, returning args."""
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def run(args):
+        """Run the tool using the parsed arguments."""
+        pass 
+
+def generate_cli_dictionary() -> Dict[str, AbstractCLI]:
+    "Add the tool-specific arguments using sub-parsers."
+
+    cli_dict = dict(keys = TOOL_NAME_LIST)
+
+    for tool_name in TOOL_NAME_LIST:
+        # Note: tool name contains a dash, while folder name uses an underscore.
+        # Generate the name of the module that contains the tool.
+        module_cli_str_list = [tool_name.replace("-", "-"), "cli"]
+        
+        # Import the module
+        module_cli = importlib.import_module('.'.join(module_cli_str_list))
+
+        # Note: the module must have a file named cli.py in the main
+        # directory, containing a class named CLI, which implements AbstractCLI.
+        cli_dict[tool_name] = module_cli.CLI()
+
+    return cli_dict
+
+
+def get_populated_argparser() -> argparse.ArgumentParser:
+    # Set up argument parser.
+
+       
+    parser = argparse.ArgumentParser(prog='cli',
+                                    description='Parses module selection (simulate or run_tests) and invokes the relevant functions',
+                                    epilog='See online documentation for more information about each function.')
+    
+    
+    parser.add_argument('-v', '--version', action='version', version = '1.1.0')
+
+
+    # Declare the existence of sub-parsers    
+    subparsers = parser.add_subparsers(
+        title = "sub-commands",
+        description = "valid simDRIFT commands",
+        dest = "tool")
+
+    for tool_name in TOOL_NAME_LIST:
+        module_argparser_str_list = [tool_name.replace("-", "_"), "argparser"]
+        module_argparse = importlib.import_module('.'.join(module_argparser_str_list))
+        subparsers = module_argparse.add_subparser_args(subparsers)
+
+    return parser
+
+def main():
+    """Parse command-line arguments and run specified tool"""
+
+
+    parser = get_populated_argparser() 
+    cli_dict = generate_cli_dictionary()
+
+    # Parse arguments
+    if len(sys.argv) > 1:
+        args = parser.parse_args(sys.argv[1:])        
+        
+        # Validate arguments.
+        args = cli_dict[args.tool].validate_args(args)
+
+        # Run the tool.
+        cli_dict[args.tool].run(args)
+    
+    return 0
+        
+if __name__ == "__main__":
+    sys.exit(main())
